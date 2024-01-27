18c8f8adf85d265bba6c41cdeb8e708cf90b32dc
Himanshu9528 <48853110+Himanshu9528@users.noreply.github.com>
2024-01-27 11:43:23 +0000

    new

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,12 @@
 console.log("hi");
+const acconttId = 3434;
+let userName = "himanshu Jadon";
+var accoutPassowrd =12345;
+accountCity = "Delhi"
+
+userName ="Himanshu Thakur";
+accoutPassowrd = 33443;
+accountCity = "Jaipur"
+
+console.table([accoutPassowrd,acconttId,accountCity,userName])
+
